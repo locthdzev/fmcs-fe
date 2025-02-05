e5db15d7a604a38381eb34be293c9563359d9c59
locthdzev <locthdev@gmail.com>
2025-02-05 22:48:50 +0700

    refactor dropdown user ui, add template user management

---
+++ src/dashboard/sidebar/icons/UsersIcon.tsx
@@ -0,0 +1,18 @@
+export function UsersIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M12 2c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4zm0 10c-4.418 0-8 3.582-8 8h16c0-4.418-3.582-8-8-8z"
+      />
+    </svg>
+  );
+}
