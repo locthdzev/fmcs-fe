1088de63185bd796fc4537349cbca33faabc97fe
Truong Vu <truongvutran00@gmail.com>
2025-03-14 03:40:07 +0700

    allow cors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['rc-util', '@ant-design/icons-svg', '@ant-design/icons', 'antd', 'rc-picker', 'rc-tree', 'rc-table'],
+  async headers() {
+    return [
+      {
+        // Áp dụng cho tất cả các routes
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
